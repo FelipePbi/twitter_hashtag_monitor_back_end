2146c1bc4e1e9b69801c4576490b22fc0ddef33d
Felipe Borges <felipexd-2011@hotmail.com>
2020-05-26 19:26:38 -0300

    default changes

--- src/configs/Api.js
+++ src/configs/Api.js
@@ -1,3 +1,3 @@
 export default {
-  baseURL: 'https://twitter-hashtag-monitor-server.herokuapp.com/api',
+  baseURL: '',
 };
--- src/configs/Redis.js
+++ src/configs/Redis.js
@@ -1,5 +1,5 @@
 export default {
-  host: 'redis-18590.c56.east-us.azure.cloud.redislabs.com',
-  port: 18590,
-  password: 'IkdfiA5uEuv4t5YPairVC40fJ8YJ3sFf',
+  host: '',
+  port: 0,
+  password: '',
 };
